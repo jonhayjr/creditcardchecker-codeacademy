f9fd9817bc944d310f7dbd71ceb55bdc983320f4
jonhayjr <jonhayjr@gmail.com>
2025-05-30 17:09:04 -0600

    Removed code used for testing

--- main.js
+++ main.js
@@ -130,5 +130,5 @@ const convertStringToNumberArray = str => {
     return stringsOnly.split('').map(Number);
     
 }
-const sampleCCString = '4,5,3,9,6,7,7,9,0,8,0,1,6,8,0,8';
-console.log(convertStringToNumberArray(sampleCCString));
+/*const sampleCCString = '4,5,3,9,6,7,7,9,0,8,0,1,6,8,0,8';
+console.log(convertStringToNumberArray(sampleCCString));*/
